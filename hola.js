8e53b3a546c993d375a05ce8cf8ac3e078a3eb53
YamiiLov3 <jmyamiletlb.44@hotmail.com>
2024-06-10 17:55:29 -0600

    Mi primer commit

---
+++ hola.js
@@ -0,0 +1,14 @@
+function validar (){
+let input numero = document .getElementById('number')
+let numero = inputNumero.validar
+
+}
+
+if (number>0) {
+
+    alert 'entonces es menor que cero'
+}
+
+else {
+    alert ('lamentablemente eres menor que cero')
+}
